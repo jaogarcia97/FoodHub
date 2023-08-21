35e210e2128af8492f937d214f8d0a6d057c79d5
Jao Garcia <josejaimefelixgarcia@gmail.com>
2023-08-22 01:07:54 +0800

    Temporary Login Screen View Created

--- FoodHub/ContentView.swift
+++ FoodHub/ContentView.swift
@@ -8,22 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack { // Use ZStack to cover the entire screen
+            Color(#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1))
             
-            Button {
-                //
-            } label: {
-                Text("Button")
-            }
+                    VStack {
+                        Image("FoodHubLogo")
+                            .resizable()
+                            .scaledToFit()
+                            .scaleEffect(0.7)
+                        
+                    
+                        TextField("Username", text: $username)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(8)
+                        
+                        SecureField("Password", text: $password)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(8)
+                        
+                        Spacer().frame(height: 40)
+                        
+                        Button(action: {
+                            // You can add your login logic here
+                            // For now, let's just print the entered credentials
+                            print("Username: \(username)")
+                            print("Password: \(password)")
+                        }) {
+                            Text("Login")
+                                                .foregroundColor(.white)
+                                                .padding(.vertical, 10)
+                                                .padding(.horizontal, 40)
+                                                .background(
+                                                    LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]),
+                                                                   startPoint: .leading,
+                                                                   endPoint: .trailing)
+                                                )
+                                                .cornerRadius(20)
+                        }
+                        
+                        Spacer().frame(height: 20)
+                        
+                        
+                        Button {
+                            //
+                        } label: {
+                            Text("Forgot password?")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(red: 1.0, green: 1.0, blue: 1.0))
+                                .bold()
+                        }
 
-            
-        }
-        .padding()
+                        Spacer().frame(height: 20)
+                        
+                        Button{
+                            //
+                        } label: {
+                            HStack{
+                                Text("Don't have an account?")
+                                    .font(.subheadline)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color(red: 1.0, green: 1.0, blue: 1.0))
+                                
+                                Text("Register Here")
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .bold()
+                                
+                            }
+                            
+                        }
+                        
+                        
+                    
+                    } //VStack End
+                    .padding()
+                }
+                .edgesIgnoringSafeArea(.all)
     }
 }
 
