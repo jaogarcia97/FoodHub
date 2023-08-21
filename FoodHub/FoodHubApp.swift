5c60e080691c98ea267b81ff4c2637f56e3c5fa0
Jao Garcia <josejaimefelixgarcia@gmail.com>
2023-08-22 00:18:44 +0800

    Initial commit

---
+++ FoodHub/FoodHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodHubApp.swift
+//  FoodHub
+//
+//  Created by Jao Garcia on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
